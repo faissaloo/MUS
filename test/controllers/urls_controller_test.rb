773bec019ecc6849a03dca8fede754aa38065a6f
faissaloo <faissaloo@gmail.com>
2018-03-31 21:44:09 +0100

    Created tests for URL shortening via interfaces and redirecting

--- test/controllers/urls_controller_test.rb
+++ test/controllers/urls_controller_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class UrlsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Can shorten url via webpage" do
+    get root_url
+    fill_in "Url", with: "https://github.com/faissaloo"
+    click_on "Shorten"
+    assert_text :visible, /st.uk\/[A-Za-z0-9\$\-\_\.\+\!\*\'\(\)\,]+/
+  end
+  
+  test "Can shorten url via POST" do
+    post shorten_url, params: {url: "2ton.com.au"}
+    assert_response :success
+  end
+  
+  test "Redirects to real page" do
+    testAddr = "www.lainchan.org"
+    get Url.shortenUrl(testAddr)
+    assert_redirected_to(testAddr)
+  end
 end
