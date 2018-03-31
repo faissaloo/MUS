8f2c4dd2e9401c536698510acfc7ac9dcd9c73b2
faissaloo <faissaloo@gmail.com>
2018-03-31 22:55:38 +0100

    Fixed capycabra testing issues

--- test/controllers/urls_controller_test.rb
+++ test/controllers/urls_controller_test.rb
@@ -1,11 +1,11 @@
 require 'test_helper'
 
-class UrlsControllerTest < ActionDispatch::IntegrationTest
+class UrlsControllerTest < Capybara::Rails::TestCase
   test "Can shorten url via webpage" do
-    get root_url
+    visit root_url
     fill_in "Url", with: "https://github.com/faissaloo"
     click_on "Shorten"
-    assert_text :visible, /st.uk\/[A-Za-z0-9\$\-\_\.\+\!\*\'\(\)\,]+/
+    assert_text :visible, /st.uk\/[A-Za-z0-9\$\-\_]+/
   end
   
   test "Can shorten url via POST" do
@@ -15,7 +15,7 @@ class UrlsControllerTest < ActionDispatch::IntegrationTest
   
   test "Redirects to real page" do
     testAddr = "www.lainchan.org"
-    get Url.shortenUrl(testAddr)
+    visit Url.shortenUrl(testAddr).sub("st.uk","")
     assert_redirected_to(testAddr)
   end
 end
