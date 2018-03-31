6e33df4dcc3fd19b3371c7abafb3dbfd518258f5
faissaloo <faissaloo@gmail.com>
2018-03-31 20:04:36 +0100

    Let's try writing tests first now, code to id conversion

--- test/models/url_test.rb
+++ test/models/url_test.rb
@@ -10,4 +10,14 @@ class UrlTest < ActiveSupport::TestCase
   test "Duplicate urls are not generated" do
     assert Url.shortenUrl("www.fsf.org") == Url.shortenUrl("www.fsf.org")
   end
+  
+  test "Ensure codes can be translated back into Ids" do
+    testid=746841600
+    assert Url.codeToId(Url.idToCode(testid))==testid
+  end
+  
+  test "Ensure ids can be translated back into codes" do
+    testcode "EternalSeptember"
+    assert Url.idToCode(Url.codeToId(testcode))==testcode
+  end
 end
