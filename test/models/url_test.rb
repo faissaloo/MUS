7f344151ce3bdc8c316704d1b0dfd5f29c5d5bc5
faissaloo <faissaloo@gmail.com>
2018-03-31 23:18:53 +0100

    Removed obsolete test

--- test/models/url_test.rb
+++ test/models/url_test.rb
@@ -1,12 +1,6 @@
 require 'test_helper'
 
 class UrlTest < ActiveSupport::TestCase
-  test "correct id generation" do
-    assert Url.idToCode(1) == "B"
-    assert Url.idToCode(2776) == "ml"
-    assert Url.idToCode(75375375197) == "X1Lv)h"
-  end
-  
   test "Duplicate urls are not generated" do
     assert Url.shortenUrl("www.fsf.org") == Url.shortenUrl("www.fsf.org")
   end
