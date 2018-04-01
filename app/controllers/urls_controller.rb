a4b6158e9353ac1e73f728ddcdbb00fed8577927
faissaloo <faissaloo@gmail.com>
2018-04-01 14:14:55 +0100

    Added validation for POST request

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,5 +1,10 @@
 class UrlsController < ApplicationController
   def new
-    @generated_url=Url.shortenUrl(params[:url])
+    begin
+      @generated_url = Url.shortenUrl(params.require(:url))
+    rescue ActionController::ParameterMissing
+      @error_msg = "Bad request: A url parameter must be provided via a POST request"
+      render status: :bad_request
+    end
   end
 end
