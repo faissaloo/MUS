e2dfc0b577446ab400e9bb50aff883edcafaec1f
faissaloo <faissaloo@gmail.com>
2018-04-01 19:02:45 +0100

    Add test for shortening invalid URLs via browser

--- test/integration/main_integration_test.rb
+++ test/integration/main_integration_test.rb
@@ -7,4 +7,11 @@ class MainIntegrationTest < Capybara::Rails::TestCase
     click_on "Shorten"
     assert_text :visible, /\/[A-Za-z0-9\$\-\_]+/
   end
+  
+  test "Invalid URLs cannot be shortened" do 
+    visit root_url
+    fill_in "Url", with: '""'
+    click_on "Shorten"
+    assert_response :bad_request
+  end
 end
